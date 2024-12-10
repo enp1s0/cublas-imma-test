1e708c04740815c9eb450edaf8e850f4613fe169
enp1s0 <ootomo.h.x86@gmail.com>
2024-12-10 23:18:30 +0900

    Add `run_cublaslt`

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <chrono>
+#include <cuda_fp8.h>
 #include <cutf/memory.hpp>
 #include <cutf/cublas.hpp>
+#include <cutf/cublaslt.hpp>
 #include <cutf/type.hpp>
 
 template <class T>
@@ -58,11 +60,8 @@ void run_cublas(
     output_data_type = CUDA_R_32F;
   }
 
-  cudaDeviceSynchronize();
-  const auto start_closk = std::chrono::system_clock::now();
-
-  for (unsigned i = 0; i < num_tests; i++) {
-    std::int32_t alpha = 1, beta = 0;
+  const OUTPUT_T alpha = 1, beta = 0;
+  const auto gemm_func = [&]() {
     CUTF_CHECK_ERROR(cublasGemmEx(
             *cublas_handle.get(),
             CUBLAS_OP_T, CUBLAS_OP_N,
@@ -75,6 +74,116 @@ void run_cublas(
             compute_type,
             CUBLAS_GEMM_DEFAULT_TENSOR_OP
             ));
+  };
+  gemm_func();
+
+  cudaDeviceSynchronize();
+  const auto start_closk = std::chrono::system_clock::now();
+
+  for (unsigned i = 0; i < num_tests; i++) {
+    gemm_func();
+  }
+
+  cudaDeviceSynchronize();
+  const auto end_closk = std::chrono::system_clock::now();
+
+  const auto elapsed_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_closk - start_closk).count() * 1e-9;
+  print_result(m_, n_, k_, elapsed_time, num_tests);
+}
+
+template <class INPUT_T, class OUTPUT_T>
+void run_cublaslt(
+    const std::size_t m_,
+    const std::size_t n_,
+    const std::size_t k_,
+    const unsigned num_tests
+    ) {
+  const auto m = round_up<INPUT_T>(m_);
+  const auto n = round_up<INPUT_T>(n_);
+  const auto k = round_up<INPUT_T>(k_);
+
+  auto mat_a_uptr = cutf::memory::get_device_unique_ptr<INPUT_T>(m * k);
+  auto mat_b_uptr = cutf::memory::get_device_unique_ptr<INPUT_T>(k * n);
+  auto mat_c_uptr = cutf::memory::get_device_unique_ptr<OUTPUT_T>(m * n);
+
+  auto cublaslt_handle = cutf::cublaslt::create_handle_unique_ptr();
+
+  const cublasOperation_t trans_A = CUBLAS_OP_T;
+  const cublasOperation_t trans_B = CUBLAS_OP_N;
+
+  auto a_desc_uptr = cutf::cublaslt::create_matrix_layout_uptr(
+      k_, m_, k, mat_a_uptr.get()
+      );
+  auto b_desc_uptr = cutf::cublaslt::create_matrix_layout_uptr(
+      k_, n_, k, mat_b_uptr.get()
+      );
+  auto c_desc_uptr = cutf::cublaslt::create_matrix_layout_uptr(
+      m_, n_, m, mat_c_uptr.get()
+      );
+
+  auto cublaslt_op_desc = cutf::cublaslt::create_matmul_desc_unique_ptr(
+      CUBLAS_COMPUTE_32F,
+      cutf::type::get_data_type<OUTPUT_T>()
+      );
+  CUTF_CHECK_ERROR(cublasLtMatmulDescSetAttribute(*cublaslt_op_desc.get(), CUBLASLT_MATMUL_DESC_TRANSA, &trans_A, sizeof(trans_A)));
+  CUTF_CHECK_ERROR(cublasLtMatmulDescSetAttribute(*cublaslt_op_desc.get(), CUBLASLT_MATMUL_DESC_TRANSB, &trans_B, sizeof(trans_A)));
+
+
+  auto cublaslt_preference_uptr = cutf::cublaslt::create_preference_unique_ptr();
+  const std::size_t worksize = 4lu << 20;
+  CUTF_CHECK_ERROR(cublasLtMatmulPreferenceSetAttribute(
+          *cublaslt_preference_uptr.get(),
+          CUBLASLT_MATMUL_PREF_MAX_WORKSPACE_BYTES,
+          &worksize,
+          sizeof(worksize)
+          ));
+
+  int returned_results = 0;
+  cublasLtMatmulHeuristicResult_t heuristic_result = {};
+  CUTF_CHECK_ERROR(cublasLtMatmulAlgoGetHeuristic(
+          *cublaslt_handle.get(),
+          *cublaslt_op_desc.get(),
+          *a_desc_uptr.get(),
+          *b_desc_uptr.get(),
+          *c_desc_uptr.get(),
+          *c_desc_uptr.get(),
+          *cublaslt_preference_uptr.get(),
+          1,
+          &heuristic_result,
+          &returned_results
+          ));
+  if (returned_results == 0) {
+    std::fprintf(stderr, "Error in cublasLtMatmulAlgoGetHeuristic\n");
+    return;
+  }
+
+  auto workspace_uptr = cutf::memory::get_device_unique_ptr<std::uint8_t>(worksize);
+
+
+  const OUTPUT_T alpha = 1, beta = 0;
+  const auto gemm_func = [&]() {
+    CUTF_CHECK_ERROR(cublasLtMatmul(
+            *cublaslt_handle.get(),
+            *cublaslt_op_desc.get(),
+            &alpha,
+            mat_a_uptr.get(), *a_desc_uptr.get(),
+            mat_b_uptr.get(), *b_desc_uptr.get(),
+            &beta,
+            mat_c_uptr.get(), *c_desc_uptr.get(),
+            mat_c_uptr.get(), *c_desc_uptr.get(),
+            &heuristic_result.algo,
+            workspace_uptr.get(),
+            worksize,
+            0
+            ));
+  };
+  gemm_func();
+
+  cudaDeviceSynchronize();
+  const auto start_closk = std::chrono::system_clock::now();
+
+  for (unsigned i = 0; i < num_tests; i++) {
+    gemm_func();
   }
 
   cudaDeviceSynchronize();
@@ -100,6 +209,10 @@ int main(int argc, char **argv) {
   if (mode == "I8I32") {
     run_cublas<signed char, int>(m, n, k, num_tests);
   } else if (mode == "F16F32") {
-    run_cublas<half, float>(m, n, k, num_tests);
+    run_cublaslt<half, float>(m, n, k, num_tests);
+  } else if (mode == "E5M2F32") {
+    run_cublaslt<__nv_fp8_e5m2, float>(m, n, k, num_tests);
+  } else if (mode == "E4M3F32") {
+    run_cublaslt<__nv_fp8_e4m3, float>(m, n, k, num_tests);
   }
 }
